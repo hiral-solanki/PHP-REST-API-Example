534a68daeba4fd630256ae1d8b977b8f1212260e
Hiral Shihora <hiral.b.solanki@gmail.com>
2024-09-19 12:55:06 +0530

    First Commit

---
+++ DbConnClass.php
@@ -0,0 +1,285 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods: PUT,GET,POST,DELETE");
+header("Access-Control-Allow-Headers: Access-Control-Allow-Headers, Authorization, Origin, X-Requested-With, Content-Type, Accept");
+require('constant.php');
+class DbConnClass{
+	private $db=DB_NAME,$host=DB_HOST,$user=DB_USER,$pass=DB_PASS;
+	protected $conn;
+	public function __construct()
+	{
+		$this->conn = new mysqli($this->host,$this->user,$this->pass,$this->db);
+		if(!$this->conn)
+		{
+			die('Connection Failed: '.mysqli_connect_errno());
+		}
+	}
+	public function trimData($val){
+		$val1 = trim($val);
+		$val2 = stripslashes($val1);
+		$val3 =htmlspecialchars($val2);
+		return $val3;
+	}
+	public function validateData($data,$type='string',$minlength=null,$maxlength=null){
+		$strlen = strlen($data);
+		if($minlength!=null && $strlen > $minlength){
+			return false;
+		}
+		else if($maxlength!=null && $strlen < $maxlength){
+			return false;
+		}
+		else if($type=='E-mail')
+		{
+			if(!filter_var((string)$data,FILTER_VALIDATE_EMAIL)){
+				return false;
+			}
+			else
+			return true;
+			
+		}
+		else if($type=='url')
+		{
+			if(!filter_var((string)$data,FILTER_VALIDATE_URL)){
+				return false;
+			}
+			else
+			return true;
+		} 
+		else if($type=='Numeric')
+		{
+			if(!filter_var((integer)$data,FILTER_VALIDATE_INT)){
+				return false;
+			}
+			else
+			return true;
+		}
+		else if($type=='alphanum')
+		{
+			if(!preg_match("/^[a-zA-Z0-9-' ]*$/",$data)){
+				return false;
+			}
+			else
+			return true;
+		}
+		else if($type=='string')
+		{
+			if(!preg_match("/^[a-zA-Z-' ]*$/",$data)){
+				return false;
+			}
+			else
+			return true;
+		}
+		else
+		{
+			return true;
+		}
+
+	}
+	public function getAllData($table,$fields="*",$where='',$groupBy='',$having='', $orderBy='',$limit='')
+	{
+		
+		$sql='select '.$fields.' from '.$table;
+		if($where!='')
+		{
+			$sql.=' where '.$where;
+		}
+		if($groupBy!='')
+		{
+			$sql.=' group by '.$groupBy;
+		}
+		if($having!='')
+		{
+			$sql.=' HAVING '.$having;
+		}
+		if($orderBy!='')
+		{
+			$sql.=' order by '.$orderBy;
+		}
+		if($limit!='')
+		{
+			$sql.=' limit '.$limit;
+		}
+		$data = $this->getResult($sql);
+		return $data;
+	}
+	public function checkData($table,$field,$where)
+	{
+		$sql='select '.$field.' from '.$table.' where '.$where;	
+		$row = $this->getRow($sql);
+		return $row;
+	}
+	private function getRow($sql)
+	{
+		$qry = $this->conn->query($sql);
+		return $tot_row=$qry->num_rows;
+	}
+	public function getUserFromLogin($table,$field,$where,$args)
+	{
+		$sql='select '.$field.' from '.$table.' where '.$where;	
+		$id = $this->getResultWithAuth($sql,$args);
+		return $id;
+	}
+	public function saveData($table,$data,$id='')
+	{
+		if(is_array($data))
+		{
+			$str='';
+			foreach($data as $k=>$v)
+			{
+				$str.= $k.'="'.$v.'",';
+			}
+			$str_qry = rtrim($str,',');
+			if($id!='')
+			{
+				$sql = "update $table set ".$str_qry." where $id";	
+			}
+			else
+			{
+				$sql = "insert into $table set ".$str_qry;	
+			}
+			return $this->saveResult($sql);
+		}
+	}
+	private function saveResult($sql)
+	{
+		$qry = $this->conn->query($sql);
+		if($qry)
+			return true;
+		else
+		return false;
+	}
+	private function getResult($sql)
+	{
+		$qry = $this->conn->query($sql);
+		$res = [];
+		$tot_row=$qry->num_rows;
+		if($tot_row > 0)
+		{
+			while($row= $qry->fetch_assoc())
+			{
+				$res[]=$row;
+			}
+		}
+		return $res;
+	}
+	private function getResultWithAuth($sql,$args)
+	{
+		$qry = $this->conn->query($sql);
+		$res = [];
+		$tot_row=$qry->num_rows;
+		if($tot_row > 0)
+		{
+			while($row= $qry->fetch_assoc())
+			{
+				$res[]=$row;
+			}
+			$res['authTok'] = $this->createKey($res,'pqr789',3600);
+		}
+		return $res;
+	}
+	function deleteData($table,$where){
+		$sql = 'DELETE FROM '.$table.' where '.$where;
+		$qry = $this->conn->query($sql);
+		if($qry){
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+	function createKey($args,$key='pqr789',$expire=null){
+		// Create token header as a JSON string
+		$header = ['typ' => 'JWT', 'alg' => 'HS256'];
+		if($expire!=null)
+		{
+			$header['expire'] = time() + $expire;
+		}
+		$header = json_encode($header);
+		// Create token payload as a JSON string
+		$payload = json_encode($args);
+
+		// Encode Header to Base64Url String
+		$base64UrlHeader = str_replace(['+', '/', '='], ['-', '_', ''], base64_encode($header));
+
+		// Encode Payload to Base64Url String
+		$base64UrlPayload = str_replace(['+', '/', '='], ['-', '_', ''], base64_encode($payload));
+
+		// Create Signature Hash
+		$signature = hash_hmac('SHA256', $base64UrlHeader . "." . $base64UrlPayload, $key, true);
+
+		// Encode Signature to Base64Url String
+		$base64UrlSignature = str_replace(['+', '/', '='], ['-', '_', ''], base64_encode($signature));
+
+		// Create JWT
+		$jwt = $base64UrlHeader . "." . $base64UrlPayload . "." . $base64UrlSignature;
+		return $jwt;
+	}
+	function verifyTok($token,$key='pqr789')
+	{
+		$token_parts = explode('.',$token);
+		$signature = hash_hmac('SHA256',$token_parts[0] . "." . $token_parts[1],$key,true);
+		$base64UrlSignature =str_replace(['+', '/', '='], ['-', '_', ''], base64_encode($signature));;
+		if($base64UrlSignature != $token_parts[2])
+		{
+			return 'Invalid';
+		}
+		$header= json_decode(base64_decode($token_parts[0]),true);
+		if(isset($header['expire']))
+		{
+			if($header['expire'] < time())
+			{
+				return 'Expire';
+			}
+		}
+		$payload = json_decode(base64_decode($token_parts[1]),true);
+		return $payload; 
+	}
+	function resizeImage($file,$ext){
+		$dir = $_SERVER['DOCUMENT_ROOT'].'/usermanagement/public/profile/';
+		$filepath = $dir.$file;
+		list($width, $height) = getimagesize($filepath);
+		// Define new dimensions (500x500 pixels)
+		$newWidth = 500;
+		$newHeight = 500;
+		if($ext=='jpeg' || $ext=='jpg')
+		{
+			$source = imagecreatefromjpeg($filepath);
+		}
+		else if($ext=='gif')
+		{
+			$source = imagecreatefromgif($filepath);
+		}
+		else if($ext=='png')
+		{
+			$source = imagecreatefrompng($filepath);
+		}
+		else
+		{	
+			$source='';
+		}
+		if($source!='')
+		{
+			// Create a new image
+			$thumb = imagecreatetruecolor($newWidth, $newHeight);
+			// Resize
+			imagecopyresized($thumb, $source, 0, 0, 0, 0, $newWidth, $newHeight, $width, $height);
+			
+			// Save the resized image
+			if($ext=='jpeg' ||$ext=='jpg')
+			{imagejpeg($thumb, $filepath, 100);}
+			if($ext=='gif')
+			{imagegif($thumb, $filepath, 100);}
+			if($ext=='jpng')
+			{imagepng($thumb, $filepath, 100);}
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+		return false;
+	}
+}
+?>
---
+++ index.php
@@ -0,0 +1,368 @@
+<?php
+$method=$_SERVER['REQUEST_METHOD'];
+if(is_array($_REQUEST) && count($_REQUEST)!=0)
+{
+	if($method!='')
+	{
+		require('DbConnClass.php');
+		$db=new DbConnClass();
+		if($method == 'POST')
+		{
+			if($_REQUEST['args']!='')
+			{
+				if($_REQUEST['args']=='authlogin')
+				{
+					$data = json_decode(file_get_contents('php://input'),true);
+					$username=$db->trimData($data['username']);
+					$upassword=$db->trimData($data['upassword']);
+					if($username !='' && $upassword!='')
+					{
+						$args = ['args' => "$username"];
+						$user = $db->getUserFromLogin('users_ang_18','uid,username','username="'.$username.'" and upassword="'.$upassword.'" and is_active=1',$args);
+						if($user)
+						{ echo json_encode(['result'=>true,'data'=>$user[0],'authTok'=>$user['authTok']]); }
+						else
+						{ echo json_encode(['result'=>false,'error'=>'Invalid Username or Password']); }					
+					}
+					else
+					{
+						echo json_encode(['result'=>false,'error'=>'Please fill required fields.(*)']);
+					}
+				}
+				else if($_REQUEST['args']=='signUpUser')
+				{
+					$data = json_decode(file_get_contents('php://input'),true);
+					$username=$db->trimData($data['username']);
+					$uremail=$db->trimData($data['uremail']);
+					$upassword=$db->trimData($data['upassword']);
+					$urole=$db->trimData($data['urole']);
+					$status = $db->trimData($data['is_active']);
+					if($username!='' && $uremail!='' && $upassword!='' && $urole!='' && $status!='')
+					{
+						$v_email = $db->validateData($uremail,'E-mail');
+						$v_user = $db->validateData($username,'string',3);
+						$isUserExist=$db->checkData('users_ang_18','username','username="'.$username.'"');
+						$isEmailExist=$db->checkData('users_ang_18','uremail','uremail="'.$uremail.'"');
+						if(!$v_email){
+							echo json_encode(['result'=>false,'error'=>'Invalid Email Address..']);
+						}
+						else if(!$v_user){
+							echo json_encode(['result'=>false,'error'=>'Invalid UserName..']);
+						}
+						//check weather username or email exists
+						else if($isUserExist==1)
+						{
+							echo json_encode(['result'=>false,'error'=>'Username already Exists']);
+						}
+						else if($isEmailExist==1)
+						{
+							echo json_encode(['result'=>false,'error'=>'E-mail already Exists']);
+						}
+						else
+						{
+							$adddata = [
+								'ufullname' => "$username",
+								'username' => "$username",
+								'uremail'=> "$uremail",
+								'upassword' => "$upassword",
+								'urole' => "$urole",
+								'udate_add'=> date('Y-m-d H:i:s'),
+								'is_active' => "$status",
+							];
+							$cid = $db->saveData('users_ang_18',$adddata);
+							if($cid){
+								echo json_encode(['result'=>true,'message'=>'User Created Successfully']);
+							}
+							else
+						   { echo json_encode(['result'=>false,'error'=>'User not Saved']);}  
+						}
+					}
+					else
+					{
+						echo json_encode(['result'=>false,'error'=>'Please fill required fields.(*)']);
+					}    
+
+				}
+				else if($_REQUEST['args']=='createUser')
+				{
+					$data = json_decode(file_get_contents('php://input'),true);
+					$ufullname=$db->trimData($data['ufullname']);
+					$username=$db->trimData($data['username']);
+					$uremail=$db->trimData($data['uremail']);
+					$upassword=$db->trimData($data['upassword']);
+					$urole=$db->trimData($data['urole']);
+					$status = $db->trimData($data['is_active']);
+					if($username!='' && $uremail!='' && $upassword!='' && $urole!='' && $status!='' && $ufullname!='')
+					{	
+						//check weather username or email exists
+						$isUserExist=$db->checkData('users_ang_18','username','username="'.$username.'"');
+						$isEmailExist=$db->checkData('users_ang_18','uremail','uremail="'.$uremail.'"');
+						//check if role and status is valid or not
+						$s_arr = [1,0];
+						$validarr = in_array($status,$s_arr);
+						$r_arr = ['Admin','User'];
+						$validrole = in_array($urole,$r_arr);
+						if(!$validarr)
+						{
+							echo json_encode(['result'=>false,'error'=>'Please Choose Valid Status']);
+						}
+						else if(!$validrole)
+						{
+							echo json_encode(['result'=>false,'error'=>'Please Choose Valid Role']);
+						}
+						else if($isUserExist==1)
+						{
+							echo json_encode(['result'=>false,'error'=>'Username already Exists']);
+						}
+						else if($isEmailExist==1)
+						{
+							echo json_encode(['result'=>false,'error'=>'E-mail already Exists']);
+						}
+						else
+						{
+							$adddata = [
+								'ufullname' => "$ufullname",
+								'username' => "$username",
+								'uremail'=> "$uremail",
+								'upassword' => "$upassword",
+								'urole' => "$urole",
+								'udate_add'=> date('Y-m-d H:i:s'),
+								'is_active' => "$status",
+							];
+							$cid = $db->saveData('users_ang_18',$adddata);
+							if($cid){
+								echo json_encode(['result'=>true,'message'=>'User Created Successfully']);
+							}
+							else
+						   { echo json_encode(['result'=>false,'error'=>'User not Saved']);}   
+						}
+					}
+					else
+					{
+						echo json_encode(['result'=>false,'error'=>'Please fill required fields.(*)']);
+					}    
+				}
+				else if($_REQUEST['args']=='editUser')
+				{
+					$data = json_decode(file_get_contents('php://input'),true);
+					$uid=$db->trimData($data['uid']);
+					$ufullname=$db->trimData($data['ufullname']);
+					$username=$db->trimData($data['username']);
+					$uremail=$db->trimData($data['uremail']);
+					$upassword=$db->trimData($data['upassword']);
+					$urole=$db->trimData($data['urole']);
+					$status=$db->trimData($data['is_active']);
+					if($username!='' && $uremail!='' && $upassword!='' && $urole!='' && $ufullname!='')
+					{
+						//check weather username or email is changed
+						$checkUser = $db->getAllData('users_ang_18','username,uremail,uimage','uid='.$uid);
+						if(is_array($checkUser) && count($checkUser) > 0)
+						{
+			
+							$old_user = $checkUser[0]['username'];
+							if($old_user!=$username) //check new and old both are same username or not
+							{
+								$isUserExist=$db->checkData('users_ang_18','username','username="'.$username.'"');//check new username already exist
+								if($isUserExist==1)
+								{
+									echo json_encode(['result'=>false,'error'=>'Username already Exists']);
+									exit;
+								}
+								else
+								{
+									$newusername = $username;
+								}
+							}
+							else
+							{
+									$newusername = $old_user;
+								
+								}
+							$old_email = $checkUser[0]['uremail'];//check new and old both are same email or not
+							if($old_email!=$uremail)
+							{
+								$isEmailExist=$db->checkData('users_ang_18','uremail','uremail="'.$uremail.'"');//check new email already exist
+								if($isEmailExist==1)
+								{
+									echo json_encode(['result'=>false,'error'=>'E-mail already Exists']);
+									exit;
+								}
+								else
+								{
+									$newemail = $uremail;
+								}
+							}
+							else
+							{
+									$newemail = $old_email;
+								
+							}
+							$editdata = [
+								'ufullname' => "$ufullname",
+								'username' => "$newusername",
+								'uremail'=> "$newemail",
+								'upassword' => "$upassword",
+								'urole' => "$urole",
+								'is_active' => "$status",
+								'udate_date'=> date('Y-m-d H:i:s')
+							];
+							$cid = $db->saveData('users_ang_18',$editdata,'uid='.$uid);
+							if($cid){
+								echo json_encode(['result'=>true,'message'=>'User Updated Successfully']);
+							}
+							else
+						   { echo json_encode(['result'=>false,'error'=>'User not Update']);} 
+						}
+						else
+						{
+							echo json_encode(['result'=>false,'message'=>'User not Found']);
+						}
+							   
+ 				
+					}
+					else
+					{
+						echo json_encode(['result'=>false,'error'=>'Please fill required fields.(*)']);
+					}    
+				}
+				else if($_REQUEST['args']=='verifytok')
+				{
+					$token = $_POST['token'];
+					$res = $db->verifyTok($token);
+					if($res=='Invalid')
+					{
+						echo json_encode(['result'=>false,'error'=>'Invalid Token']);
+					}
+					elseif($res=='Expire')
+					{
+						echo json_encode(['result'=>false,'error'=>'Login Expire']);
+					}
+					else
+					{
+						echo json_encode(['result'=>true,'resdata'=>$res]);
+					}
+				}
+			    else if($_REQUEST['args']=='uploadfile')
+				{
+					$uid = $_POST['uid'];
+					$oldimage = $_POST['old_img'];
+					$file_size=($_FILES['file']['size']/1024);
+					$file_type=$_FILES['file']['type'];
+					$path_parts = pathinfo($_FILES["file"]["name"]);
+					$extenstion = $path_parts['extension'];
+					$filename = $path_parts['basename'];
+					if($file_size > 2000)
+					{
+						echo json_encode(['result'=>false,'error'=>'Maximum File size is 2MB your size is ..']);
+					}
+					else if(strlen($filename) > 25){
+						echo json_encode(['result'=>false,'error'=>'Invalid filename! Maximum 25 characters allowed.']);
+					}
+					else if(!preg_match('/^[\w|\-\.]+$/', $filename)){
+						echo json_encode(['result'=>false,'error'=>'Invalid filename!  a-zA=Z-_ allowed only.']);
+					}
+					else if($extenstion=='jpeg' ||$extenstion=='jpg' || $extenstion=='gif' || $extenstion=='png')
+					{
+						$dir = $_SERVER['DOCUMENT_ROOT'].'/usermanagement/public/profile/';
+						$file = $_FILES['file']['name'];						
+						$newFileName= $uid."_".$filename;
+						$file_tmp = $_FILES['file']['tmp_name'];
+						$uploadfile = move_uploaded_file($file_tmp,$dir.$newFileName);
+						$db->resizeImage($newFileName,$extenstion);
+						if($uploadfile)
+						{
+							//delete old image
+							if($oldimage!='') {
+								if(file_exists($dir . $oldimage)) //check file exist or not
+								{ 
+									unlink($dir.$oldimage);
+								}
+							} 
+							$resize= $db->resizeImage($newFileName,$extenstion);
+							if($resize)
+							{
+								$editdata = [
+									'uimage'=>"$newFileName",
+								];
+								$cid = $db->saveData('users_ang_18',$editdata,'uid='.$uid);
+							    if($cid){
+									echo json_encode(['result'=>true,'message'=>'File Uploaded Successfully','file'=>$newFileName]);
+								}
+								else
+							    {
+								 echo json_encode(['result'=>false,'error'=>'File not Found']);
+							    } 
+							}
+							else
+							{
+								echo json_encode(['result'=>false,'error'=>'File not found']);
+							}
+						}
+						else
+						{
+							echo json_encode(['result'=>false,'error'=>'File not found']);
+						}
+
+					}
+					else
+					{
+						echo json_encode(['result'=>false,'error'=>'Invalid File Type, Only JPG,GIF,PNG Files allowed']);
+					}
+				}
+				else if($_REQUEST['args']=='getuser')
+				{
+					$uid = $_POST['uid'];
+					$user = $db->getAllData('users_ang_18','*','uid='.$uid);
+					if($user)
+					{ echo json_encode(['result'=>true,'data'=>$user]); }
+					else
+					{ echo json_encode(['result'=>false,'error'=>'Invalid Request']); }
+				}
+				else if($_REQUEST['args']=='delUser'){
+					$uid = $_POST['uid'];
+					$delData = $db->deleteData('users_ang_18','uid='.$uid);
+					if($delData){
+						echo json_encode(['result'=>true,'message'=>'User Delete Successfully']);
+					}
+					else
+					{
+						echo json_encode(['result'=>false,'error'=>'Error.. Delete user Fails!']);
+					}
+				}
+			}		
+		}
+		else if ($method == 'GET')
+		{
+				if($_REQUEST['args']!='')
+				{
+					if($_REQUEST['args']=='alluser')
+					{
+						//$page=isset($_POST['page'])?$_POST['page']:1;
+						//$limit=5;
+						//$offset = ($page -1) * $limit;
+						$user = $db->getAllData('users_ang_18','*');
+						//print_r($user);
+						//exit;
+						if($user)
+						{ echo json_encode(['result'=>true,'data'=>$user]); }
+						else
+						{ echo json_encode(['result'=>false,'error'=>'Invalid Request']); }
+					}
+
+				}
+		}
+		else
+		{
+			echo json_encode(['result'=>false,'error'=>'Invalid Method']);
+		}
+	}
+	else
+	{
+		echo json_encode(['result'=>false,'error'=>'Invalid Method Pass']);
+	}
+}
+else
+{
+    echo json_encode(['result'=>false,'error'=>'Invalid Request']);
+}
+?>
